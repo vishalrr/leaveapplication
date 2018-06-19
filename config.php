081d585c17db6e8d1a2029735ce134384d9323f9
vishalrr <34366547+vishalrr@users.noreply.github.com>
2018-06-19 18:11:43 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "root";
-    $datbase = "mansa_project";
+    $host = "fdb21.awardspace.net";
+    $user = "2753521_mansa";
+    $password = "vishal@123";
+    $datbase = "2753521_mansa";
     $connec=mysqli_connect($host,$user,$password,$datbase);
     
 ?>
